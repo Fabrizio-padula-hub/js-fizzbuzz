6772777c800a3e320549b6b20abe037bd5e3d4d0
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-07 14:56:55 +0100

    fizzbuzz adventure

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per 
+// i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+// Prendere i numeri da 1 a 100
+    // Controllare che i numeri siano multipli sia per 3 che per 5 'FizzBuzz'
+    // Controllare che i numeri siano multipli di 3 'Fizz'
+    // Controllare che i numeri siano multipli di 5 'Buzz'
+
+// Stampare in console
