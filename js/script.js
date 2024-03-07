06a6419389fa4c9af23da6bc075e6a165afdb51b
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-07 15:34:49 +0100

    feat: add for loop

--- js/script.js
+++ js/script.js
@@ -5,8 +5,27 @@
 
 
 // Prendere i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    
     // Controllare che i numeri siano multipli sia per 3 che per 5 'FizzBuzz'
     // Controllare che i numeri siano multipli di 3 'Fizz'
     // Controllare che i numeri siano multipli di 5 'Buzz'
+    let multipleOfFizzBuzz
+    if(i % 3 === 0 && i % 5 === 0){
+        multipleOfFizzBuzz = 'FizzBuzz';
+
+    }else if(i % 3 === 0){
+        multipleOfFizzBuzz = 'Fizz';
+
+    }else if(i % 5 === 0){
+        multipleOfFizzBuzz = 'Buzz';
+
+    }else{
+        multipleOfFizzBuzz = i;
+    }
+    console.log(multipleOfFizzBuzz)
+
+};
+    
 
 // Stampare in console
